dacedd33716eaa2132eeb24cf9c56c404a09a454
DaDDy <blacklistuser420@gmail.com>
2022-04-09 09:06:48 +0630

    darkmode

--- bootstrap-exc/src/script.js
+++ bootstrap-exc/src/script.js
@@ -66,3 +66,32 @@ var waypoint = new Waypoint({
   },
   offset: '50%'
 })
+
+// _________________________________________
+const changeThemeToDark = ()=>{
+  document.documentElement.setAttribute('data-theme','dark')
+  localStorage.setItem('data-theme','dark');
+}
+const changeThemeToLight = ()=>{
+  document.documentElement.setAttribute('data-theme','light')
+  localStorage.setItem('data-theme','light');
+}
+
+
+let changeTheme = document.getElementById('changeTheme');
+changeTheme.addEventListener('change',()=>{
+  let theme = localStorage.getItem('data-theme')
+  if(theme === 'dark'){
+    changeThemeToLight();
+  }else{
+    changeThemeToDark();
+  }
+})
+
+let theme = localStorage.getItem('data-theme')
+if(theme === 'dark'){
+  changeThemeToDark();
+  changeTheme.setAttribute('checked','checked')
+}else{
+  changeThemeToLight();
+}
